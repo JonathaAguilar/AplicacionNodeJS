c14692fa4d3945fb923b017f46e1e9259503516b
JONATHAN JESUS <tiojona20@gmail.com>
2024-02-22 11:05:01 -0700

    Se realizaron las practiccas

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import json from 'body-parser';
 import path from 'path';
 import misRutas from './router/index.js';
 
-const puerto = 3000;
+const puerto = 80;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
